65e1fbb75ebb62a936d92bf5e4135501dfddfb0d
Marcus <marcus.gamboa@gmail.com>
2024-09-13 10:42:42 -0400

    Add Grid to ProductList

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -11,6 +11,11 @@ export default function ProductList({ title, items }: Props) {
     <div className="space-y-4">
       <h3 className="text-3xl font-bold">{title}</h3>
       {items.length === 0 && <NoResults />}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {items.map(item => {
+          return <div key={item.id}>{item.name}</div>;
+        })}
+      </div>
     </div>
   );
 }
